874655f93b2081001912b000956d1713a448efd0
Jon Clerck <j.m.clerck@icloud.com>
2016-05-08 21:49:21 +0100

    Add push registration
    Add functionality to push a subscription to a database

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 'use strict';
 
+let registerPushSubscription = subscription => {
+  var headers = new Headers();
+  headers.append('Content-Type', 'application/json');
+
+  return fetch('https://registration.jclerck.io/push', {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({
+      endpoint: subscription.endpoint,
+    }),
+  });
+};
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('sw.js');
 
   navigator.serviceWorker.ready.then(registration => {
     registration.pushManager.getSubscription()
       .then(subscription => {
-        return subscription ? subscription : registration.pushManager.subscribe({
-          userVisibleOnly: true,
-        });
+        return subscription ?
+          registerPushSubscription(subscription) :
+          registration.pushManager.subscribe({
+            userVisibleOnly: true,
+          }).then(registerPushSubscription);
       });
   });
 }
