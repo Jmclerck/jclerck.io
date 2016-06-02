2dc563fbe362edc64a6a3e21d6010d1c449a709a
Jon Clerck <j.m.clerck@icloud.com>
2016-06-02 12:50:15 +0100

    Fix docker sources.list

--- config/provision.sh
+++ config/provision.sh
@@ -15,7 +15,7 @@ apt-get update && sudo apt-get install -y apt-transport-https ca-certificates
 
 ## Docker reop
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo deb https://apt.dockerproject.org/repo ubuntu-precise main > /etc/apt/sources.list.d/passenger.list
+echo deb https://apt.dockerproject.org/repo ubuntu-xenial main > /etc/apt/sources.list.d/docker.list
 
 ## Docker
 apt-get update && apt-get -y install docker-engine
