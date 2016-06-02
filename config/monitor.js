69518df299e0316afd067a0758062e1b8c590c28
Jon Clerck <j.m.clerck@icloud.com>
2016-06-02 11:29:45 +0100

    Improve development workflow

--- config/monitor.js
+++ config/monitor.js
@@ -2,32 +2,55 @@
 
 'use strict';
 
-import spawn from 'child_process';
+import proc from 'child_process';
 import nodemon from 'nodemon';
+import npm from 'npm';
 
-let ls = spawn.exec(`passenger start --app-type node --environment ${process.env.ENV} --startup-file dist/app.js`);
+var args = [
+  '--app-type node',
+  `--environment ${process.env.ENV}`,
+  '--startup-file dist/app.js',
+];
 
-ls.stdout.on('data', (data) => {
-  console.log(data);
+var opts = {
+  cwd: '.',
+  shell: true
+};
+
+let passenger = proc.spawn('passenger start', args, opts);
+
+passenger.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
 });
 
-ls.stderr.on('data', (data) => {
-  console.log(data);
+passenger.stderr.on('data', (data) => {
+  console.log(`stderr: ${data}`);
 });
 
-ls.on('close', (code) => {
-  console.log(`Child exited with code ${code}`);
+passenger.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
 });
 
-if (process.env.ENV !== 'production') {
+if (process.env.ENV === 'production'){
+  // In production we only want to kick off the build step once
+  npm.load({}, function (err) {
+    if (!err) {
+      npm.commands.run([
+        'build'
+      ]);
+    }
+  });
+} else {
+  /*
+   *  In development we want to kick off the build step once,
+   *  then again when changes occur, and reload passenger
+   */
   nodemon({
     watch: [
       'src',
     ],
-    exec: 'npm run build; passenger-config restart-app /root/passenger',
-  });
-
-  nodemon.on('start', function () {
+    exec: 'npm run build; passenger-config restart-app .',
+  }).on('start', function () {
     console.log('App has started');
   }).on('quit', function () {
     console.log('App has quit');
