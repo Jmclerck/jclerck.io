8fa653baa64b40f43d8bf437c4078942e32cc667
Jon Clerck <j.m.clerck@icloud.com>
2017-03-15 22:04:05 +0000

    Various updates 😇

--- public/src/cv.js
+++ public/src/cv.js
@@ -1,33 +1,29 @@
 var visited = false;
 
 if (document.cookie !== '') {
-  var list = document.cookie.split(';');
-
-  list.forEach(function (item) {
-    var item = item.split('=');
-
+  document.cookie.split(';').forEach(function(item) {
+    item = item.split('=');
     if (item[0].trim() === 'accepted') {
       visited = !!item[1];
     }
   });
 }
 
-document.addEventListener('click', function (event) {
+document.addEventListener('click', function(event) {
   var bgColour = event.target.getAttribute('data-bg-colour');
   document.documentElement.style.setProperty('--bg-colour', bgColour);
-
   var fgColour = event.target.getAttribute('data-fg-colour');
   document.documentElement.style.setProperty('--fg-colour', fgColour);
 });
 
 if (!visited) {
-  var cookieAccept = function (event) {
+  var cookieAccept = function(event) {
     if (event.code && event.code === 'Enter' || event.type === 'click') {
-      document.cookie="accepted=true";
+      document.cookie = 'accepted=true';
       document.querySelector('.cookies').remove();
       document.removeEventListener('keydown', cookieAccept);
     }
-  }
+  };
 
   document.querySelector('.accept').addEventListener('click', cookieAccept);
   document.addEventListener('keydown', cookieAccept);
@@ -36,9 +32,11 @@ if (!visited) {
     var synth = window.speechSynthesis;
     var intro = document.querySelector('.intro');
     var text = `Welcome, this is my CV. If you don't want to hear this again, please press Enter to accept cookies.`;
-    var voices = synth.getVoices().filter(voice => voice.lang === navigator.language && voice.default);
+    var voices = synth
+      .getVoices()
+      .filter(voice => voice.lang === navigator.language && voice.default);
 
-    text.split(/\s+/g).forEach(function (word) {
+    text.split(/\s+/g).forEach(function(word) {
       var sp = document.createElement('span');
       sp.innerText = word;
       sp.className = `${word.match(/\w+/g).join('')}-spoken`;
@@ -47,17 +45,17 @@ if (!visited) {
 
     var utterance = new SpeechSynthesisUtterance(text);
     utterance.voice = voices[0];
-    utterance.onboundary = function (event) {
+    utterance.onboundary = function(event) {
       var boundaryStart = event.charIndex;
       var boundaryEnd = event.currentTarget.text.indexOf(' ', event.charIndex);
 
-      var nextWord = event.currentTarget.text.slice(boundaryStart, boundaryEnd)
+      var nextWord = event.currentTarget.text.slice(boundaryStart, boundaryEnd);
       var nextClass = nextWord.match(/\w+/g).join('');
       var nextElem = intro.querySelector(`.${nextClass}-spoken`);
       if (nextElem) {
         nextElem.classList.remove(nextElem.classList);
       }
-    }
+    };
 
     synth.speak(utterance);
   }
--- public/sw.js
+++ public/sw.js
@@ -1,85 +1,85 @@
 `use strict`;
 
-const cacheVersion = `1.2.5`;
-const cacheName = `${self.location.host}-${cacheVersion}`
+const cacheVersion = `2.0.0`;
+const cacheName = `${self.location.host}-${cacheVersion}`;
 const cacheContents = [
-	`/`,
-	`index.html`,
-	`login.html`,
-	`downloads/`,
-	`dist/`,
-	`images/`,
-	`styles/`,
+  `/`,
+  `index.html`,
+  `login.html`,
+  `translations.json`,
+  `downloads/`,
+  `dist/`,
+  `images/`,
+  `styles/`,
+  `private/the-wedding.html`,
 ];
 
 self.onactivate = event => {
-	const clear = () => {
-		caches.keys().then(cacheNames => {
-			const cacheDeletions = cacheNames
-				.filter(cache => cache !== cacheName)
-				.map(cache => caches.delete(cache));
+  const clear = () => {
+    caches
+      .keys()
+      .then(cacheNames => {
+        const cacheDeletions = cacheNames
+          .filter(cache => cache !== cacheName)
+          .map(cache => caches.delete(cache));
 
-			return Promise.all(cacheDeletions);
-		}).then(() => self.clients.claim());
-	};
+        return Promise.all(cacheDeletions);
+      })
+      .then(() => self.clients.claim());
+  };
 
-	event.waitUntil(clear);
+  event.waitUntil(clear);
 };
 
 self.oninstall = event => {
-	const cache = () => {
-		caches.open(cacheName)
-			.then(cache => cache.addAll(cacheContents))
-			.then(() => self.skipWaiting());
-	};
+  const cache = () => {
+    caches
+      .open(cacheName)
+      .then(cache => cache.addAll(cacheContents))
+      .then(() => self.skipWaiting());
+  };
 
-	event.waitUntil(cache);
+  event.waitUntil(cache);
 };
 
 self.onfetch = event => {
-	const offlineResponse = () => {
-		const isImage = event.request.headers.get(`Accept`).includes(`image`);
+  const offlineResponse = () => {
+    const isImage = event.request.headers.get(`Accept`).includes(`image`);
 
-		return isImage ? caches.match(`images/404.png`) : new Response(`<h1>Offline</h1>`, {
-			headers: {
-				'Content-Type': `text/html`,
-			},
-		});
-	};
+    return isImage
+      ? caches.match(`images/404.png`)
+      : new Response(`<h1>Offline</h1>`, { headers: { 'Content-Type': `text/html` } });
+  };
 
-	const cacheResponse = response => {
-		caches.open(cacheName)
-			.then(cache => cache.put(event.request, response));
+  const cacheResponse = response => {
+    caches.open(cacheName).then(cache => cache.put(event.request, response));
 
-		return response.clone();
-	};
+    return response.clone();
+  };
 
-	const respondFromNetwork = () => fetch(event.request).then(cacheResponse);
+  const respondFromNetwork = () => fetch(event.request).then(cacheResponse);
 
-	const respondFromCache = () => caches.match(event.request).then(response => response ? response : offlineResponse());
+  const respondFromCache = () =>
+    caches.match(event.request).then(response => response ? response : offlineResponse());
 
-	event.respondWith(respondFromNetwork(event).catch(respondFromCache));
+  event.respondWith(respondFromNetwork(event).catch(respondFromCache));
 };
 
 self.onpush = event => {
-	if (!self.Notification || self.Notification.permission !== `granted`) {
-		return;
-	}
+  if (!self.Notification || self.Notification.permission !== `granted`) {
+    return;
+  }
 
-	let data = {};
+  let data = {};
 
-	if (event.data) {
-		data = event.data.json();
-	}
+  if (event.data) {
+    data = event.data.json();
+  }
 
-	const title = data.title || `notification`;
-	const body = data.message || `notification`;
-	const icon = `images/icons/notification.png`;
+  const title = data.title || `notification`;
+  const body = data.message || `notification`;
+  const icon = `images/icons/notification.png`;
 
-	self.registration.showNotification(title, {
-		body,
-		icon,
-	});
+  self.registration.showNotification(title, { body, icon });
 };
-
 // self.onsync = event => {};
