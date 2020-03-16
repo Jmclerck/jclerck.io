9639588d774c12d650d796e66b875003f85419ba
Jon Clerck <j.m.clerck@icloud.com>
2020-03-16 21:35:45 +0000

    Redesign v2
    Signed-off-by: Jon Clerck <j.m.clerck@icloud.com>

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = "jclerck-v2";
+
+const urlsToCache = ["/", "index.html", "primary.css"];
+
+self.addEventListener("install", function(event) {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches
+      .keys()
+      .then(keys =>
+        Promise.all(
+          keys.map(key => {
+            if (!CACHE_NAME.includes(key)) {
+              return caches.delete(key);
+            }
+          })
+        )
+      )
+      .then(() =>
+        caches.open(CACHE_NAME).then(cache => {
+          return cache.addAll(urlsToCache);
+        })
+      )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request).then(response => {
+        if (!response || !response.ok) {
+          return response;
+        }
+
+        const responseToCache = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
+  );
+});
